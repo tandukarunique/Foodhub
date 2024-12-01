a564a2bc85d493932a3a2247f14afbe2adbc9082
tandukaruniqur <uniquetandukar143@gmail.com>
2024-12-01 17:08:12 +0545

    navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,57 @@
-import React from 'react'
-import './Navbar.css'
-import logo from "../../assets/assests/logo.png"
-import searchicon from "../../assets/assests/searchicon.jpg"
-import basketicon from "../../assets/assests/basketicon.png"
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/assests/logo.png";
+import searchicon from "../../assets/assests/searchicon.jpg";
+import basketicon from "../../assets/assests/basketicon.png";
 const Navbar = () => {
+  const [menu, setmenu] = useState("home");
   return (
-    <div className='navbar'>
-      <img src={logo} style={{height:"200px",width:"20%"}} alt='logo'/>
-<ul className='navbar-menu'>
-  <li>Home</li>
-  <li>Menu</li>
-  <li>Mobile App</li>
-  <li>Contact us</li>
-  </ul>     
-  <div className='navbar-right'>
-    <img src={searchicon} style={{height:"80px",width:"auto"}} alt='searchicon'/>
-    <div className="narbar-search-icon">
-      <img src={basketicon} style={{height:"30px",width:"auto"}} alt='basketicon'/>
-      <div className='dot'></div>
-      <button>Sign in</button>
-
+    <div className="navbar">
+      <img src={logo} style={{ height: "200px", width: "230px" }} alt="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setmenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setmenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setmenu("MobileApp")}
+          className={menu === "MobileApp" ? "active" : ""}
+        >
+          MobileApp
+        </li>
+        <li
+          onClick={() => setmenu("Contactus")}
+          className={menu === "Contactus" ? "active" : ""}
+        >
+          Contactus
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img
+          src={searchicon}
+          style={{ height: "80px", width: "auto" }}
+          alt="searchicon"
+        />
+        <div className="navbar-search-icon">
+          <img
+            src={basketicon}
+            style={{ height: "30px", width: "auto" }}
+            alt="basketicon"
+          />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
     </div>
-    </div> 
-    </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
