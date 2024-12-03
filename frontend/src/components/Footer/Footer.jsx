75f26f33824654f7dbdee00580d03dc19c2b420e
tandukaruniqur <uniquetandukar143@gmail.com>
2024-12-03 21:21:26 +0545

    footer added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Footer.css";
 import { assests } from "../../assets/assests";
-
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { CgMail } from "react-icons/cg";
 
 const Footer = () => {
   return (
@@ -9,15 +11,39 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
           <img src={assests.logo} alt="" />
-          <p>
+          <p className="footerp " style={{ marginTop: "-29px",padding:"10px" }}>
             Discover the world on a plate! At FoodFusion, we bring you a curated
             selection of mouthwatering dishes from across the globe, made with
             the freshest ingredients and served with love.{" "}
           </p>
-          <div className="footer-social-icons"></div>
+          <div className="footer-social-icons">
+            <a href="https://www.facebook.com/">
+              <FaFacebook />
+            </a>
+            <a href="https://www.instagram.com/">
+              <FaInstagram />
+            </a>
+            <a href="mailto:your-email@example.com">
+              <CgMail />
+            </a>
+          </div>
         </div>
-        <div className="footer-content-right"></div>
-        <div className="footer-content-center"></div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>980-3503348</li>
+            <li>contact@foodhub.com</li>
+          </ul>
+        </div>
       </div>
     </div>
   );
